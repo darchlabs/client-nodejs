ab5db35a8857d55917684f39e240b6f4c10afd73
Camilo Acuña <camilo.aku@gmail.com>
2023-03-30 06:38:22 +0700

    refactor(src): add new implementation for client and remove unused files

--- src/types.ts
+++ src/types.ts
@@ -1,25 +1,10 @@
-export type _synchronizer = {
-  network: Network;
-  address: string;
-  raw?: string;
-};
+export type Network = "ethereum" | "rinkeby" | "polygon" | "mumbai" | "avalanche" | "goerli" | "none";
+export type CronjobStatus = "idle" | "running" | "stopping" | "stopped" | "error";
+export type EventStatus = "synching" | "running" | "stopped" | "error";
+export type Sort = "asc" | "ASC" | "desc" | "DESC";
 
-export type SynchronizerFormData = _synchronizer & {
-  abi: string;
-  event: string;
-};
-
-export type SynchronizerBase = {
-  latestBlockNumber: number;
-  abi: Abi;
-};
-
-export type Synchronizer = SynchronizerBase & {
-  createdAt: string;
-  updatedAt: string;
-};
-
-export type AbiInput = {
+export type Input = {
+  id?: string;
   indexed: boolean;
   internalType: string;
   name: string;
@@ -27,51 +12,57 @@ export type AbiInput = {
 };
 
 export type Abi = {
+  id?: string;
+  name: string;
+  type: string;
   anonymous: boolean;
-  inputs: AbiInput[];
-  name: string;
-  type: string;
+  inputs: Input[];
 };
 
-export type abiMethod = {
-  inputs: [];
-  name: string;
-  outputs: [Output];
-  stateMutability: string;
-  type: string;
+export type Event = {
+  id: string;
+  network: Network;
+  address: string;
+  latestBlockNumber: number;
+  nodeURL: string;
+  status: EventStatus;
+  error: string;
+  abi: Abi;
+  createdAt: string;
+  updatedAt: string;
 };
 
-type Output = {
-  internalType: string;
-  name: string;
-  type: string;
+export type EventInput = {
+  network: Network;
+  nodeURL: string;
+  abi: Abi;
 };
-
-export type abiEvent = {
-  anonymous: false;
-  inputs: abiMethod[];
-  name: string;
-  type: string;
-};
-
-export type Network =
-  | "ethereum"
-  | "rinkeby"
-  | "polygon"
-  | "mumbai"
-  | "avalanche"
-  | "goerli"
-  | "none";
-
-export type CronjobStatus =
-  | "idle"
-  | "running"
-  | "stopping"
-  | "stopped"
-  | "error"
-  | "sync";
 
 export type Cronjob = {
   status: CronjobStatus;
   seconds: number;
+  error: string;
+};
+
+export type Pagination = {
+  page: number;
+  limit: number;
+  totalElements: number;
+  totalPages: number;
+  sort: Sort;
+};
+
+export type EventData<T> = {
+  id: string;
+  eventId: string;
+  tx: string;
+  blockNumber: string;
+  data: T;
+  createdAt: string;
+};
+
+export type Options = {
+  limit?: number;
+  page?: number;
+  sort?: Sort;
 };
