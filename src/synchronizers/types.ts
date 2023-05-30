a314da47aa4d568f3c72ac0767475c540b4888c7
Camilo Acuña <camilo.aku@gmail.com>
2023-05-30 22:18:47 +0800

    refactor(types): update abi

--- src/synchronizers/types.ts
+++ src/synchronizers/types.ts
@@ -1,23 +1,8 @@
 import { Network } from "../util";
+import { Abi, EventAbi } from "../util/abi";
 
 export type CronjobStatus = "idle" | "running" | "stopping" | "stopped" | "error";
 export type EventStatus = "synching" | "running" | "stopped" | "error";
-
-export type Input = {
-  id?: string;
-  indexed: boolean;
-  internalType: string;
-  name: string;
-  type: string;
-};
-
-export type Abi = {
-  id?: string;
-  name: string;
-  type: string;
-  anonymous: boolean;
-  inputs: Input[];
-};
 
 export type Event = {
   id: string;
@@ -27,7 +12,7 @@ export type Event = {
   nodeURL: string;
   status: EventStatus;
   error: string;
-  abi: Abi;
+  abi: EventAbi;
   createdAt: string;
   updatedAt: string;
 };
@@ -38,12 +23,6 @@ export type EventInput = {
   abi: Abi;
 };
 
-export type Cronjob = {
-  status: CronjobStatus;
-  seconds: number;
-  error: string;
-};
-
 export type EventData<T> = {
   id: string;
   eventId: string;
@@ -51,4 +30,10 @@ export type EventData<T> = {
   blockNumber: string;
   data: T;
   createdAt: string;
+};
+
+export type Cronjob = {
+  status: CronjobStatus;
+  seconds: number;
+  error: string;
 };
