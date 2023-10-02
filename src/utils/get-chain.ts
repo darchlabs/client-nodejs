43d145e0696dde6feeec7a9656a8eb07626ba55f
Camilo Acuña <camilo.aku@gmail.com>
2023-10-03 00:34:04 +0200

    refactor(utils): change folder name to 'utils'

---
+++ src/utils/get-chain.ts
@@ -0,0 +1,15 @@
+import { ChainIds, Network } from "./network";
+
+export const GetNetworkId = (name: Network): number => {
+  return ChainIds[name];
+};
+
+export const GetNetworkName = (id: number): Network | undefined => {
+  for (const network in ChainIds) {
+    if (ChainIds[network as Network] === id) {
+      return network as Network;
+    }
+  }
+
+  return undefined;
+};
---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+export * from "./get-chain";
+export * from "./subset";
+export * from "./network";
+export * from "./pagination";
