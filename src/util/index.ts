cf59773e886e9f72613596b7b372053f125a7c41
Camilo Acuña <camilo.aku@gmail.com>
2023-10-03 00:33:39 +0200

    refactor: delete unused files

--- src/util/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./abi";
-export * from "./get-chain";
-export * from "./network";
-export * from "./pagination";
-export * from "./subset";
-export * from "./evm-trigger-method";
-export * from "./listen-server"
